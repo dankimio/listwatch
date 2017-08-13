9913a106d50af6ab04cca3f06eaf45fcf110276f
Dan Kim <itsdanya@gmail.com>
2017-08-13 23:04:43 +0200

    Add views and assets for lists

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on('turbolinks:load', function() {
+  // Add position of the movie in the list to span with "position" class
+  $.each($('.movie-list-item'), function(index, value) {
+    console.log($(value));
+    $(value).find('.position').text(index + 1 + '.');
+  });
+});
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
   def show
+    @movies = Movie.all
   end
 end
