9e9971a225ca25315326bbdd6db6631a42a52774
Dan Kim <itsdanya@gmail.com>
2017-08-17 22:41:03 +0200

    Render posters preview in lists#index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def small_poster_url(tmdb_poster_path)
+    poster_url(tmdb_poster_path, 185)
+  end
+
+  def medium_poster_url(tmdb_poster_path)
+    poster_url(tmdb_poster_path, 300)
+  end
+
+  def poster_url(path, size)
+    "https://image.tmdb.org/t/p/w#{size}#{path}"
+  end
 end
